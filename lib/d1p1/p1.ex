2bab73f3d7e92de459d09e7fc6c228346e55c729
Bill Nunney <bill@bigtallbill.com>
2024-12-02 06:56:46 -0500

    Refactor D1p1.P1 module for clarity and consistency
    - Renamed functions for better readability: `calc_distances` to `calculate_absolute_differences`, `parse_input` to `parse_input_to_pairs`, and `split_lists` to `sort_and_zip_pairs`.
    - Updated corresponding test cases to reflect the new function names and improved descriptions.
    - Enhanced input parsing and distance calculation logic for improved clarity.

--- lib/d1p1/p1.ex
+++ lib/d1p1/p1.ex
@@ -1,23 +1,23 @@
 defmodule D1p1.P1 do
   def run do
     input = File.read!("lib/d1p1/input.txt")
-    IO.inspect(calc_distances(input) |> Enum.sum())
+    IO.inspect(calculate_absolute_differences(input) |> Enum.sum())
   end
 
-  def calc_distances(input) do
+  def calculate_absolute_differences(input) do
     input
-    |> parse_input()
-    |> split_lists()
+    |> parse_input_to_pairs()
+    |> sort_and_zip_pairs()
     |> Enum.map(fn {l, r} -> abs(l - r) end)
   end
 
-  def parse_input(input) do
+  def parse_input_to_pairs(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1, ~r/\s+/, trim: true))
   end
 
-  def split_lists(original_list) do
+  def sort_and_zip_pairs(original_list) do
     {left, right} =
       Enum.map(original_list, fn [l, r] -> {String.to_integer(l), String.to_integer(r)} end)
       |> Enum.unzip()
--- test/d1p1/p1_test.exs
+++ test/d1p1/p1_test.exs
@@ -2,21 +2,21 @@ defmodule D1p1.P1Test do
   use ExUnit.Case
   doctest D1p1.P1
 
-  test "parse_input splits input string into list of integers" do
+  test "parse_input_to_pairs splits input string into list of pairs" do
     input = "123   456\n234   567\n"
     expected = [["123", "456"], ["234", "567"]]
-    assert D1p1.P1.parse_input(input) == expected
+    assert D1p1.P1.parse_input_to_pairs(input) == expected
   end
 
-  test "split_lists splits a list into two lists" do
+  test "sort_and_zip_pairs sorts and zips two lists" do
     original_list = [["123", "456"], ["789", "012"]]
     expected = [{123, 12}, {789, 456}]
-    assert D1p1.P1.split_lists(original_list) == expected
+    assert D1p1.P1.sort_and_zip_pairs(original_list) == expected
   end
 
-  test "calc_distances calculates the distances between two lists" do
+  test "calculate_absolute_differences calculates the absolute differences between two lists" do
     input = "5   10\n10   15\n10   5"
     expected = [0, 0, 5]
-    assert D1p1.P1.calc_distances(input) == expected
+    assert D1p1.P1.calculate_absolute_differences(input) == expected
   end
 end
