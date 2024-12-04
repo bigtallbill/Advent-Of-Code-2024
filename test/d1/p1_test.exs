46d87c6d2208f2dc44eb95ee57d7aa97cda43655
Bill Nunney <bill@bigtallbill.com>
2024-12-04 06:06:45 -0500

    Fix tests

--- test/d1/p1_test.exs
+++ test/d1/p1_test.exs
@@ -5,18 +5,18 @@ defmodule D1.P1Test do
   test "parse_input_to_pairs splits input string into list of pairs" do
     input = "123   456\n234   567\n"
     expected = [["123", "456"], ["234", "567"]]
-    assert D1.P1.parse_input_to_pairs(input) == expected
+    assert D1.P1.parse_input_to_pairs(input) |> Enum.to_list() == expected
   end
 
   test "sort_and_zip_pairs sorts and zips two lists" do
     original_list = [["123", "456"], ["789", "012"]]
     expected = [{123, 12}, {789, 456}]
-    assert D1.P1.sort_and_zip_pairs(original_list) == expected
+    assert D1.P1.sort_and_zip_pairs(original_list) |> Enum.to_list() == expected
   end
 
   test "calculate_absolute_differences calculates the absolute differences between two lists" do
     input = "5   10\n10   15\n10   5"
     expected = [0, 0, 5]
-    assert D1.P1.calculate_absolute_differences(input) == expected
+    assert D1.P1.calculate_absolute_differences(input) |> Enum.to_list() == expected
   end
 end
