bcb35c60c089f9043654abd5e788bab9428c3ed9
Bill Nunney <me@billnunney.dev>
2024-12-12 07:59:23 -0500

    Finished day 6 part 1

--- lib/d6/p1.ex
+++ lib/d6/p1.ex
@@ -11,25 +11,27 @@ defmodule D6.P1 do
 
   def run_with_input(input) do
     input
-    |> String.split("\n", trim: true)
-    # TODO: Implement day 6 specific logic here
+    |> build_grid()
+    |> simulate()
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   def build_grid(input) do
     grid = input
     |> String.split("\n", trim: true)
     |> Enum.with_index()
-    |> Enum.reduce([], fn {line, y}, acc ->
+    |> Enum.reduce(%{}, fn {line, y}, acc ->
       line
       |> String.graphemes()
       |> Enum.with_index()
       |> Enum.reduce(acc, fn {char, x}, acc ->
-        [{x, y, char} | acc]
+        Map.put(acc, {x, y}, char)
       end)
     end)
 
-    {max_x, _, _} = Enum.max_by(grid, fn {x, _, _} -> x end, fn -> 0 end)
-    {_, max_y, _} = Enum.max_by(grid, fn {_, y, _} -> y end, fn -> 0 end)
+    {{max_x, _}, _} = Enum.max_by(grid, fn {{x, _}, _} -> x end, fn -> 0 end)
+    {{_, max_y}, _} = Enum.max_by(grid, fn {{_, y}, _} -> y end, fn -> 0 end)
 
     start = get_start(grid)
 
@@ -38,16 +40,74 @@ defmodule D6.P1 do
 
   def get_start(grid) do
     grid
-    |> Enum.find(nil, fn {_, _, char} -> char == "^" end)
+    |> Enum.find(nil, fn {{_, _}, char} -> char == "^" end)
     |> case do
       nil -> raise "No start found"
-      {x, y, _} -> {x, y}
+      {{x, y}, _} -> {x, y}
     end
   end
 
+  @doc """
+  Simulate the grid.
+  From the start position, move forward (always starting with up) until you hit a blockage. Then turn 90 degrees clockwise.
+  Repeat until you hit the edge of the grid. Keep track of the positions you pass through.
+  """
   def simulate(grid) do
     matrix = grid.matrix
+    start = grid.start
+    size = grid.size
 
+    simulate_step(matrix, start, size, :up, [start])
+    |> Enum.reverse()
+  end
+
+  @doc """
+  Simulates a single step in the grid. Recursively calls itself until it hits a blockage or the edge of the grid.
+  """
+  def simulate_step(matrix, position, size, direction, visited) do
+    new_position = move_forward(position, direction)
+
+    {newX, newY} = new_position
+    {maxX, maxY} = size
+
+    if newX < 0 or newX > maxX or newY < 0 or newY > maxY do
+      visited
+    else
+      if is_blockage?(matrix, new_position) do
+        case direction do
+          :up -> simulate_step(matrix, position, size, :right, visited)
+          :right -> simulate_step(matrix, position, size, :down, visited)
+          :down -> simulate_step(matrix, position, size, :left, visited)
+          :left -> simulate_step(matrix, position, size, :up, visited)
+        end
+      else
+        simulate_step(matrix, new_position, size, direction, [new_position | visited])
+      end
+    end
+  end
+
+  @doc """
+  Moves the position forward in the given direction.
+  """
+  def move_forward({x, y}, direction) do
+    case direction do
+      :up -> {x, y - 1}
+      :right -> {x + 1, y}
+      :down -> {x, y + 1}
+      :left -> {x - 1, y}
+    end
+  end
+
+  @doc """
+  Check if a position is a blockage.
+  A blockage is defined by the # character.
+  """
+  def is_blockage?(matrix, {x, y}) do
     matrix
+    |> Map.get({x, y}, nil)
+    |> case do
+      nil -> false
+      char -> char == "#"
+    end
   end
 end
--- test/d6/p1_test.exs
+++ test/d6/p1_test.exs
@@ -2,14 +2,7 @@ defmodule D6.P1Test do
   use ExUnit.Case
 
   test "run_test" do
-    assert D6.P1.run_test() == 0  # Update this with the expected result
-  end
-
-  test "run_with_input" do
-    input = """
-    # Add your test input here
-    """
-    assert D6.P1.run_with_input(input) == 0  # Update this with the expected result
+    assert D6.P1.run_test() == 41
   end
 
   test "build_grid" do
@@ -18,17 +11,17 @@ defmodule D6.P1Test do
     ..#
     ^#.
     """
-    assert D6.P1.build_grid(input) == %{:size => {2, 2}, :start => {0, 2}, :matrix => [
-        {2, 2, "."},
-        {1, 2, "#"},
-        {0, 2, "^"},
-        {2, 1, "#"},
-        {1, 1, "."},
-        {0, 1, "."},
-        {2, 0, "."},
-        {1, 0, "."},
-        {0, 0, "#"}
-      ]
+    assert D6.P1.build_grid(input) == %{:size => {2, 2}, :start => {0, 2}, :matrix => %{
+        {2, 2} => ".",
+        {1, 2} => "#",
+        {0, 2} => "^",
+        {2, 1} => "#",
+        {1, 1} => ".",
+        {0, 1} => ".",
+        {2, 0} => ".",
+        {1, 0} => ".",
+        {0, 0} => "#"
+      }
     }
   end
 
@@ -40,5 +33,19 @@ defmodule D6.P1Test do
     """
     |> D6.P1.build_grid()
     |> D6.P1.simulate() == [{0, 2}, {0, 1}, {1, 1}, {1, 2}]
+  end
+
+  test "is_blockage?" do
+    assert D6.P1.is_blockage?(%{
+      {0, 0} => "#",
+      {1, 0} => ".",
+      {2, 0} => "."
+    }, {1, 0}) == false
+
+    assert D6.P1.is_blockage?(%{
+      {0, 0} => "#",
+      {1, 0} => ".",
+      {2, 0} => "."
+    }, {0, 0}) == true
   end
 end
